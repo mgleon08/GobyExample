0b61f97c0f574ef46a1f17e9c62bf9a81302fda6
Leon Ji <mgleon08@gmail.com>
2018-10-23 22:29:51 +0800

    033.tickers

---
+++ 033.tickers/main.go
@@ -0,0 +1,33 @@
+// [Timers](timers) are for when you want to do
+// something once in the future - _tickers_ are for when
+// you want to do something repeatedly at regular
+// intervals. Here's an example of a ticker that ticks
+// periodically until we stop it.
+
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+
+	// Tickers use a similar mechanism to timers: a
+	// channel that is sent values. Here we'll use the
+	// `range` builtin on the channel to iterate over
+	// the values as they arrive every 500ms.
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for t := range ticker.C {
+			// 每 500 毫秒就印出
+			fmt.Println("Tick at", t)
+		}
+	}()
+
+	// Tickers can be stopped like timers. Once a ticker
+	// is stopped it won't receive any more values on its
+	// channel. We'll stop ours after 1600ms.
+	// 這裡停止 1600 毫秒，不然會直接結束 main()
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+}
